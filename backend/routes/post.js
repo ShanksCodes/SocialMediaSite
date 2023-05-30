08735f8d57c1512bbdbb24158c63a2ff8eefdbc6
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-05-30 22:38:21 +0530

    z

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createPost, likeAndUnlikePost, deletePost, getPostOfFollowing, updateCaption } = require("../controllers/post");
+const { createPost, likeAndUnlikePost, deletePost, getPostOfFollowing, updateCaption, commentOnPost, deleteComment } = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 const router = express.Router();
 
@@ -12,4 +12,9 @@ router
 .delete(isAuthenticated, deletePost);
 
 router.route("/posts").get(isAuthenticated, getPostOfFollowing);
+
+router.route("/post/comment/:id")
+.put(isAuthenticated, commentOnPost)
+.delete(isAuthenticated, deleteComment);
+
 module.exports = router;
