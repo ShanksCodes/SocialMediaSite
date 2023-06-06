63a04d03d7a594a9fae9b393739a932c68dd69ce
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-06-07 01:30:02 +0530

    pre update post

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,8 @@ require("dotenv").config({path: "backend/config/config.env"});
 }
 
 //using middleware
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended:true}));
 app.use(cookieParser());
 
 //importing modules
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadUser());
-  }, []);
+  }, [dispatch]);
 
 
   const { isAuthenticated } = useSelector((state) => state.user);
